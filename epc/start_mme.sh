49d049775357826e3c7f1011be41632e9a9b7edb
Binh Nguyen <binh@cs.utah.edu>
2017-02-01 13:47:39 -0700

    Run modified MME on SGW

--- epc/start_mme.sh
+++ epc/start_mme.sh
@@ -11,7 +11,7 @@ sudo /opt/OpenEPC/bin/mme.kill.sh
 
 sudo cp $XML/mme.xml /opt/OpenEPC/etc/
 
-cd $SIMECA_EPC/wharf_rel5
+cd $SIMECA_EPC/wharf
 
 screen -S mme -L -d -m -h 10000 /bin/bash -c "./wharf -f /opt/OpenEPC/etc/mme.xml"
 screen -wipe
